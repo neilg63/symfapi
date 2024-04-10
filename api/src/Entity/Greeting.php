0394562506ea7236e043495fa7909a8abcb0816b
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2024-04-10 18:05:38 +0100

    Edited Greeting Entity File

--- api/src/Entity/Greeting.php
+++ api/src/Entity/Greeting.php
@@ -28,8 +28,19 @@ class Greeting
     #[Assert\NotBlank]
     public string $name = '';
 
+    /**
+     * A nice person
+     */
+    #[ORM\Column(type: 'integer')]
+    public ?int $age = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
     }
 }
